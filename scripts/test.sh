46fb8ad992ee2abf39fe75eb76505c3676fa7ca2
dmi3y <lapshukov@gmail.com>
2016-06-23 22:30:25 -0700

    better tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd ./tests
-find ./templates-raw/ -name '*.hbs' -exec sh -c '"../bin/handlebamdle.js" < "${1}" > "./templates-bundled/$(basename ${1/hbs/js})"' -- {} \;
-node ./index-node.js
+pushd ./tests
+  mkdir -p ./templates-bundled
+  find ./templates-raw/ -name '*.hbs' -exec sh -c '"../bin/handlebamdle.js" < "${1}" > "./templates-bundled/$(basename ${1/hbs/js})"' -- {} \;
+  node ./index-node.js
+popd
